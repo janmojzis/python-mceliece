6369ec15a7671bbaf8ac57bc45f0500a0f7430db
Jan Mojžíš <jan.mojzis@gmail.com>
2024-08-27 15:00:47 +0200

    autogen/init update

--- src/mceliece/__init__.py
+++ src/mceliece/__init__.py
@@ -30,22 +30,18 @@ from .kem import mceliece6960119
 from .kem import mceliece6960119f
 from .kem import mceliece6960119pc
 from .kem import mceliece6960119pcf
-
 from .kem import mceliece6688128
 from .kem import mceliece6688128f
 from .kem import mceliece6688128pc
 from .kem import mceliece6688128pcf
-
 from .kem import mceliece8192128
 from .kem import mceliece8192128f
 from .kem import mceliece8192128pc
 from .kem import mceliece8192128pcf
-
 from .kem import mceliece460896
 from .kem import mceliece460896f
 from .kem import mceliece460896pc
 from .kem import mceliece460896pcf
-
 from .kem import mceliece348864
 from .kem import mceliece348864f
 from .kem import mceliece348864pc
